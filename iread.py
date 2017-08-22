bd8511a3fc8583070f5b7576f6ba856beb2fbcb8
Lucas Moura <lucas.moura128@gmail.com>
2017-08-21 21:27:03 -0300

    Add option to check article

--- iread.py
+++ iread.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 import argparse
+import sqlitedict
+
+
+DATABASE_PATH = 'articles.sqlite'
+
+
+def check_article(article):
+    if article.endswith('.pdf'):
+        article = article[:-4]
+
+    with sqlitedict.SqliteDict(DATABASE_PATH) as dataset:
+        is_read = dataset.get(article, False)
+
+    return is_read
 
 
 def create_argument_parser():
@@ -17,11 +31,17 @@ def create_argument_parser():
     return parser
 
 
+def check_args(user_args):
+    if user_args.check is not None:
+        is_read = check_article(user_args.check)
+        print(is_read)
+
+
 def main():
     parser = create_argument_parser()
 
     user_args = parser.parse_args()
-    print(user_args)
+    check_args(user_args)
 
 
 if __name__ == '__main__':
